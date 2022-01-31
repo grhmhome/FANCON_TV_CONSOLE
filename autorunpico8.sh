abe9b0eb86408edf7b09b2ef6eb96af701463d76
grhmhome <mrbetaabc@gmail.com>
2022-01-30 17:20:14 -0800

    Update autorunpico8.sh

--- autorunpico8.sh
+++ autorunpico8.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#change this if your floppy drive is located elsewhere besides /dev/sda
 sudo mount /dev/sda /media/floppy
 if [ -f "/media/floppy/run.p8.png" ]
 then
 exec /home/pi/pico-8/pico8 -root_path /media/floppy -run /media/floppy/run.p8.png &
 else
-exec /home/pi/pico-8/pico8 /media/floppy -splore &
+exec /home/pi/pico-8/pico8 -root_path /media/floppy -splore &
 fi
 exit 0
