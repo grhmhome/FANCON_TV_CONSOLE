653cc1c80f4bfa2ba01807b30d586da22f7b1ce3
grhmhome <mrbetaabc@gmail.com>
2022-01-30 16:01:15 -0800

    Update autorunpico8.sh
    Will mount floppies instead.

--- autorunpico8.sh
+++ autorunpico8.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [ -f "/media/usb0/run.p8.png" ]
+sudo mount /dev/sda /media/floppy
+if [ -f "/media/floppy/run.p8.png" ]
 then
-exec /home/pi/pico-8/pico8 -run /media/usb0/run.p8.png &
+exec /home/pi/pico-8/pico8 -root_path /media/floppy -run /media/floppy/run.p8.png &
 else
-exec /home/pi/pico-8/pico8 -splore &
+exec /home/pi/pico-8/pico8 /media/floppy -splore &
 fi
 exit 0
